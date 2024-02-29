22c7ac6af2c3265a08a73c77c19cdad4552e190d
Nirmean <ana_noona@live.com>
2024-02-29 21:47:03 +0200

    removed h1 and replaced with form title component

--- src/pages/SavingsGoals.jsx
+++ src/pages/SavingsGoals.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import UserInput from '../components/UserInput';
+import FormTitle from '../components/FormTitle';
 
 function SavingsGoals({ updateUserData }) {
     const navigate = useNavigate();
@@ -21,83 +22,14 @@ function SavingsGoals({ updateUserData }) {
     };
 
     return (
-        <>
-            <h1 className="mb-10">Your Saving Goals:</h1>
-
+        <div>
+            <FormTitle title='Your Savings Goals:' />
             <p className="italics">Simply enter the item or goal name, set the target amount, and let Insight Wallet guide you towards realizing your aspirations..</p>
-
-            <div className="flex p-2 justify-start">
-
-            <input
-                type="text"
-                name="savingsItem1"
-                className="rounded-3xl border-0 py-4 pl-7 pr-20 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black-300 sm:text-md sm:leading-6"
-                placeholder="Holiday"
-            />
-
-                <div className="relative">
-                    <div className="absolute inset-y-0 left-0 flex items-center pl-3">
-                        <span className="text-gray-500 sm:text-md">£</span>
-                </div>
-
-                <input
-                        type="text"
-                        name="Holiday"
-                        className="rounded-3xl border-0 py-4 pl-7 pr-20 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black-300 sm:text-md sm:leading-6"
-                        placeholder="0.00"
-                        onChange={handleChange}
-                />
-                </div>
-
-            </div>
-
-            <div className="flex p-2 justify-start">
-
-                <label
-                    htmlFor="savingItem2"
-                    className="text-lg font-medium leading-10 px-10 py-2 mx-5 border-0 ring-1 ring-inset ring-gray-300 w-48 savingItem2"
-                >
-                    Car
-                </label>
-
-                <div className="relative">
-                    <div className="absolute inset-y-0 left-0 flex items-center pl-3">
-                        <span className="text-gray-500 sm:text-md">£</span>
-                    </div>
-                    <input
-                        type="text"
-                        name="price"
-                        className="rounded-3xl border-0 py-4 pl-7 pr-20 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black-300 sm:text-md sm:leading-6"
-                        placeholder="0.00"
-                    />
-                </div>
-
-            </div>
-
-            <div className="flex p-2 justify-start">
-                <label
-                    htmlFor="savingItem3"
-                    className="text-lg font-medium leading-10 px-10 py-2 mx-5 border-0 ring-1 ring-inset ring-gray-300 w-48 savingItem3"
-                >
-                    Other
-                </label>
-                <div className="relative">
-                    <div className="absolute inset-y-0 left-0 flex items-center pl-3">
-                        <span className="text-gray-500 sm:text-md">£</span>
-                    </div>
-                    <input
-                        type="text"
-                        name="price"
-                        className="rounded-3xl border-0 py-4 pl-7 pr-20 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black-300 sm:text-md sm:leading-6"
-                        placeholder="0.00"
-                    />
-                </div>
-            </div>
-
-            <button className="button" onClick={handleSubmit}>
-                Next
-            </button>
-        </>
+            <UserInput incomeTitle='Holiday' value={saving.item1} handleChange={handleChange} />
+            <UserInput incomeTitle='Car' value={saving.item2} handleChange={handleChange} />
+            <UserInput incomeTitle='Laptop' value={saving.item3} handleChange={handleChange} />
+            <FormButton onclick={handleSubmit} title='Next' />
+        </div>
     );
 }
 
