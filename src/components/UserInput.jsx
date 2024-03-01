bf3f6400095a81f6e5fdcb2f8f14944e5ea45077
Paulina Siwko <pau.siwko@gmail.com>
2024-03-01 13:12:23 +0100

    added useEffect to display total sum in income page

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function UserInput({incomeTitle, value, handleChange}) {
+export default function UserInput({incomeTitle, handleChange}) {
     return (
         <>
         <div className="flex p-2 justify-start">
@@ -9,7 +9,7 @@ export default function UserInput({incomeTitle, value, handleChange}) {
             <div className="absolute inset-y-0 left-0 flex items-center pl-3">
                 <span className="text-gray-500 sm:text-md">£</span>
             </div>
-            <input type="number" className="rounded-3xl border-0 py-4 pl-7 pr-20 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black-300 sm:text-md sm:leading-6" placeholder="0.00" value={value} onChange={handleChange} />
+            <input type="number" className="rounded-3xl border-0 py-4 pl-7 pr-20 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black-300 sm:text-md sm:leading-6" placeholder="0.00" onChange={(e) => handleChange(incomeTitle,e)} />
             </div>
         </div>
         </>
--- src/pages/Income.jsx
+++ src/pages/Income.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import UserInput from '../components/UserInput';
 import FormTitle from './FormTitle';
@@ -7,37 +7,75 @@ import AddIncome from '../components/AddIncome';
 
 function Income({ updateUserData }) {
   const navigate = useNavigate();
-  const [income, setIncome] = useState({ salary: '', sideHustle: '', other: '', additionalIncome: [] });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setIncome(prevIncome => ({
-      ...prevIncome,
-      [name]: value
-    }));
+  const [salaryInput, setSalaryInput] = useState(0);
+  const [sideHustleInput, setSideHustleInput] = useState(0);
+  const [otherInput, setOtherInput] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  const handleMainInputChange = (name, e) => {
+    const { value } = e.target;
+    const valueNumber = parseFloat(value);
+
+    switch (name) {
+      case 'Salary':
+        setSalaryInput(valueNumber);
+        break;
+      case 'Side Hustle':
+        setSideHustleInput(valueNumber);
+        break;
+      case 'Other':
+        setOtherInput(valueNumber);
+        break;
+      default:
+        break;
+    }
+
   };
 
+  useEffect(() => {
+    const totalSum = salaryInput + sideHustleInput + otherInput;
 
-   const handleAddIncome = () => {
-    setIncome(prevIncome => ({
-      ...prevIncome,
-      additionalIncome: [...prevIncome.additionalIncome, { title: '', amount: '' }]
-    }));
-  };
+    setTotal(totalSum);
+  }, [salaryInput, sideHustleInput, otherInput]);
 
-  const handleAdditionalIncomeChange = (index, e) => {
-    const { name, value } = e.target;
-    setIncome(prevIncome => ({
-      ...prevIncome,
-      additionalIncome: prevIncome.additionalIncome.map((item, i) =>
-        i === index ? { ...item, [name]: value } : item
-      )
-    }));
-  };
+  // const [income, setIncome] = useState({ salary: '', sideHustle: '', other: '', additionalIncome: [] });
+
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setIncome(prevIncome => ({
+  //     ...prevIncome,
+  //     [name]: value
+  //   }));
+  // };
+
+
+  //  const handleAddIncome = () => {
+  //   setIncome(prevIncome => ({
+  //     ...prevIncome,
+  //     additionalIncome: [...prevIncome.additionalIncome, { title: '', amount: '' }]
+  //   }));
+  // };
+
+  // const handleAdditionalIncomeChange = (index, e) => {
+  //   const { name, value } = e.target;
+  //   setIncome(prevIncome => ({
+  //     ...prevIncome,
+  //     additionalIncome: prevIncome.additionalIncome.map((item, i) =>
+  //       i === index ? { ...item, [name]: value } : item
+  //     )
+  //   }));
+  // };
 
 
   const handleSubmit = () => {
-    updateUserData({ income });
+    const incomeData = {
+      salary: salaryInput,
+      sideHustle: sideHustleInput,
+      other: otherInput,
+    };
+
+    updateUserData({ income: incomeData });
     navigate('/budget');
   };
 
@@ -45,26 +83,26 @@ function Income({ updateUserData }) {
     <>
     <div>
       <FormTitle title='Your Income:' />
-      <UserInput incomeTitle='Salary' value={income.salary} handleChange={handleChange}/>
-      <UserInput incomeTitle='Side Hustle' income={income.sideHustle} handleChange={handleChange}/>
-      <UserInput incomeTitle='Other' income={income.other} handleChange={handleChange}/>
+      <UserInput incomeTitle='Salary' handleChange={handleMainInputChange}/>
+      <UserInput incomeTitle='Side Hustle' handleChange={handleMainInputChange}/>
+      <UserInput incomeTitle='Other' handleChange={handleMainInputChange}/>
 
-      {income.additionalIncome && income.additionalIncome.map((item, index) => (
+      {/* {income.additionalIncome && income.additionalIncome.map((item, index) => (
         <div key={index} className="flex p-2 justify-start">
           <AddIncome index={index} />
         </div>
-      ))}
+      ))} */}
 
       <div className='flex flex-row justify-center'>
         <FormButton onClick={handleSubmit} title='Next' />
-        <FormButton onClick={handleAddIncome} title='Add new income' />
+        {/* <FormButton onClick={handleAddIncome} title='Add new income' /> */}
       </div>
 
     </div>
 
       <div className='flex p-2'>
-            <label className="text-lg font-medium leading-10 px-10 py-2 mx-5 border-0 ring-1 ring-inset ring-gray-300 w-48 incomeTitle">Total</label>
-            <label className="text-lg font-medium leading-10 px-10 py-2 mx-5 border-0 ring-1 ring-inset ring-gray-300 w-48 incomeTitle">SUMA</label>
+            <label className="text-lg font-medium leading-10 px-10 py-2 mx-5 border-0 ring-1 ring-inset ring-gray-300 w-48 bg-white rounded-3xl">Total</label>
+            <label className="text-lg font-medium leading-10 px-10 py-2 mx-5 border-0 ring-1 ring-inset ring-gray-300 w-48 bg-white rounded-3xl">{total.toFixed(2)}</label>
       </div>
     </>
   );
