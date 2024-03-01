056229a6221a3af23d66a81f1eec669449f58736
Pawel <pochmarek@gmail.com>
2024-03-01 20:51:20 +0000

    Fixed savings page. Added new item button

--- src/pages/SavingsGoals.jsx
+++ src/pages/SavingsGoals.jsx
@@ -1,40 +1,96 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import UserInput from '../components/UserInput';
+import FormTitle from './FormTitle';
+import FormButton from '../components/FormButton';
+import AddIncome from '../components/AddIncome';
 
 function SavingsGoals({ updateUserData }) {
     const navigate = useNavigate();
-    const [savingsGoals, setSavingsGoals] = useState({
-        Holiday: "",
-        Car: "",
-        Laptop: "",
-    });
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setSavingsGoals({ ...savingsGoals, [name]: value });
-    };
+    const [holidayInput, setHolidayInput] = useState(0);
+    const [houseInput, setHouseInput] = useState(0);
+    const [carInput, setOtherInput] = useState(0);
+    const [additionalSavings, setAdditionalSavings] = useState([]);
 
-    const handleSubmit = () => {
-        if (typeof updateUserData === 'function') {
-            updateUserData({ SavingsGoals: savingsGoals });
-            navigate("/dashboard");
-        } else {
-            console.error("updateUserData is not a function");
-        }
-    };
+    const handleMainInputChange = (name, e) => {
+        const { value } = e.target;
+        let valueNumber = parseFloat(value);
     
+        if (isNaN(valueNumber)) {
+          valueNumber = 0;
+        }
+    
+        switch (name) {
+          case 'Holiday':
+            setHolidayInput(valueNumber);
+            break;
+          case 'House':
+            setHouseInput(valueNumber);
+            break;
+          case 'Other':
+            setOtherInput(valueNumber);
+            break;
+          default:
+            break;
+        }
+     };
 
-    return (
+     const handleAddSavings = () => {
+       
+        setAdditionalSavings([
+          ...additionalSavings,  { title: '', amount: 0 }
+        ])
+    };
+        
+
+      const handleAdditionalSavingGoals = (index, e) => {
+        const { value } = e.target;
+        let valueNumber = parseFloat(value);
+    
+        if(isNaN(valueNumber)) {
+          valueNumber = 0;
+        }
+        
+        setAdditionalSavings(
+          (previousAdditionalSavings) => previousAdditionalSavings.map((obj, i) => i === index ? {...obj, amount: valueNumber} : obj) 
+        )
+      };
+
+      const handleSubmit = () => {
+        const savingsData = {
+          holiday: holidayInput,
+          house: houseInput,
+          car: carInput,
+        };
+    
+        updateUserData({ savings: savingsData });
+        navigate('/lasttransactionsform');
+      };
+    
+      return (
+        <>
         <div>
-            <h2>Your Savings Goals:</h2> 
-            <p className="italics">Simply enter the item or goal name, set the target amount, and let Insight Wallet guide you towards realizing your aspirations.</p>
-            <UserInput inputTitle='Holiday' value={savingsGoals.Holiday} handleChange={handleChange} /> 
-            <UserInput inputTitle='Car' value={savingsGoals.Car} handleChange={handleChange} /> 
-            <UserInput inputTitle='Laptop' value={savingsGoals.Laptop} handleChange={handleChange} /> 
-            <button onClick={handleSubmit}>Next</button> 
+          <FormTitle title='Your Saving Goals:' />
+          <UserInput incomeTitle='Holiday:' handleChange={handleMainInputChange}/>
+          <UserInput incomeTitle='House:' handleChange={handleMainInputChange}/>
+          <UserInput incomeTitle='Car:' handleChange={handleMainInputChange}/>
+    
+          {additionalSavings.map((item, index) => (
+            <div key={index} className="flex p-2 justify-start">
+              <AddIncome index={index} onChange={handleAdditionalSavingGoals}/>
+            </div>
+          ))}
+    
+          <div className='flex flex-row justify-center'>
+            <FormButton onClick={handleAddSavings} title='Add New Saving Goal' />
+            <FormButton onClick={handleSubmit} title='Next' />
+          </div>
+    
         </div>
-    );
-}
+
+        </>
+      );
+    }
 
 export default SavingsGoals;
