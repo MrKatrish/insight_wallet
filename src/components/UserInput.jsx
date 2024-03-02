ef33d2661ba48d96b1aea0cb09ff966676325cba
Paulina Siwko <pau.siwko@gmail.com>
2024-03-02 17:21:22 +0100

    added paragraphs to all form's pages and adjusted tailwind classes

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function UserInput({incomeTitle, handleChange}) {
     return (
         <>
-        <div className="flex p-2 justify-start">
+        <div className="flex p-2 justify-center">
             <label className="text-lg font-medium leading-10 px-10 py-2 mx-5 border-0 ring-1 ring-inset ring-gray-300 w-48 bg-white rounded-3xl">{incomeTitle}</label>
             <div className="relative">
             <div className="absolute inset-y-0 left-0 flex items-center pl-3">
