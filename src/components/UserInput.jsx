048b0569256a0539dd2a61f1e71b6cc6e04611d9
Nirmean <ana_noona@live.com>
2024-03-04 23:55:58 +0200

    removed duplicate income title column in user input

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -5,12 +5,6 @@ export default function UserInput({labelTitle, labelAmount, handleChange, handle
         <>
         <div className="flex p-2 justify-center">
             <input type="text" className="text-lg font-medium leading-10 px-10 py-2 mx-5 border-0 ring-1 ring-inset ring-gray-300 w-48 bg-white rounded-3xl text-center" placeholder="Title" value={labelTitle} onChange={(e) => handleLabelTitleChange(id, e)}></input>
-            <input
-            type="text"
-            className="text-lg font-medium leading-10 px-10 py-2 mx-5 border-0 ring-1 ring-inset ring-gray-300 w-48 bg-white rounded-3xl"
-            placeholder={incomeTitle}
-            onChange={(e) => handleChange(incomeTitle, e)}
-            />
             <div className="relative">
             <div className="absolute inset-y-0 left-0 flex items-center pl-3">
                 <span className="text-gray-500 sm:text-md">£</span>
