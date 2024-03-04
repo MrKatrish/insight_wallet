f82b7279db02acc5142ea1650a5d620e91bffd30
Nirmean <ana_noona@live.com>
2024-03-04 23:37:13 +0200

    styled X buttons in user input

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -11,7 +11,7 @@ export default function UserInput({labelTitle, labelAmount, handleChange, handle
             </div>
             <input type="number" className="rounded-3xl border-0 py-4 pl-7 pr-20 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400" placeholder="0.00" value={labelAmount === 0 ? "" : labelAmount} onChange={(e) => handleChange(id,e)} />
             </div>
-            <button onClick={() => handleDelete(id)} className="text-lg font-medium border-0 px-5 mx-5 ring-1 ring-inset ring-gray-300 rounded-3xl bg-white text-red-400">X</button>
+            <button onClick={() => handleDelete(id)} className="text-sm font-medium border-0 w-10 h-10 px-2 mx-5 mt-2 ring-1 ring-inset ring-gray-300 rounded-3xl bg-gray-100 text-gray-400">X</button>
         </div>
         </>
     );
