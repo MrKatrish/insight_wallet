a56f0bd495ef6e99585f0ad9a003e7ecd0736c41
Nirmean <ana_noona@live.com>
2024-03-04 16:47:28 +0200

    aligned navbar to left of page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,35 +1,43 @@
-import React from 'react';
-import Sidebar from '../components/Sidebar';
-import Footer from '../components/Footer';
-import UserBadge from '../components/UserBadge';
-import profileImage from '../Assets/jonas-kakaroto.jpg'; // Import the profile picture
-import Chart from '../components/ChartPie';
-import CurrencyConverter from '../components/CurrencyExchanger';
-import BarChart from '../components/BarChart';
+import React from "react";
+import Sidebar from "../components/Sidebar";
+import Footer from "../components/Footer";
+import UserBadge from "../components/UserBadge";
+import profileImage from "../Assets/jonas-kakaroto.jpg"; // Import the profile picture
+import Chart from "../components/ChartPie";
+import CurrencyConverter from "../components/CurrencyExchanger";
+import BarChart from "../components/BarChart";
 
-
-const Dashboard = () => { 
+const Dashboard = () => {
   const user = {
-    username: 'Jonas Kakaroto',
+    username: "Your Profile",
     profilePicture: profileImage,
   };
   return (
     <>
-      <Sidebar />
-      <div className="flex-1 overflow-y-auto bg-gray-100">
+    <div className="flex">
+      {/* Sidebar */}
+      <div className="w-1/4 min-h-screen bg-gray-800 text-white"> 
+        <Sidebar className="flex"/>
+      </div>
+
+      {/* Main Content */}
+      <div className="fw-3/4 min-h-screen bg-gray-100">
         <div className="container mx-auto px-4 py-8">
-          {/* Content */}
+          {/* Welcome Content */}
           <h1 className="text-3xl font-bold mb-8">Dashboard</h1>
           <p>Welcome to your dashboard!</p>
-          <UserBadge username={user.username} profilePicture={user.profilePicture} />
+          <UserBadge
+            username={user.username}
+            profilePicture={user.profilePicture}
+          />
         </div>
-       
+        <Chart />
+        <CurrencyConverter /> {/* Include CurrencyConverter component */}
+        <BarChart />
+        <Footer />
       </div>
-      <Chart />
-      <CurrencyConverter /> {/* Include CurrencyConverter component */}
-      <Chart/>
-      <BarChart/>
-      <Footer />
+      </div>
+
     </>
   );
 };
