df3de9a7bc167c8a3c39ecae0494f220ae3985eb
Nirmean <ana_noona@live.com>
2024-03-06 00:17:49 +0200

    added titles to sections

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -20,11 +20,13 @@ const Dashboard = () => {
           <div className="bg-white bg-opacity-50 rounded-xl p-10 grid grid-cols-2 grid-rows-3 gap-4">
             {/* Tile 1 - Bar Chart */}
             <div className="bg-white rounded-xl shadow-md p-4 col-span-1">
+              <h2>Financial Overview</h2>
               <BarChart />
             </div>
             
             {/* Tile 2 - Currency Converter */}
             <div className="bg-white rounded-xl shadow-md p-4 col-span-1">
+              <h2>Currency Converter</h2>
               <CurrencyConverter />
             </div>
 
@@ -35,16 +37,19 @@ const Dashboard = () => {
 
             {/* Tile 4 - Pie Chart */}
             <div className="bg-white rounded-xl shadow-md p-4 col-span-1">
+              <h2>Monthly Budget</h2>
               <Chart />
             </div>
 
             {/* Tile 5 - Last Transactions */}
             <div className="bg-white rounded-xl shadow-md p-4 col-span-1">
+              <h2>Latest Transactions</h2>
               <LastTransactions />
             </div>
 
             {/* Tile 6 - Savings Goals (Placeholder) */}
             <div className="bg-white rounded-xl shadow-md p-4 col-span-1">
+              <h2>Savings Progress</h2>
               {/* here add SavingsGoals component*/}
             </div>
           </div>
