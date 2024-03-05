5dfe3c283ebb31e575ee1899c81491d557712da7
Michal Paleczny <paleczny.michal@gmail.com>
2024-03-05 19:44:03 +0000

    added logo to all pages

--- src/components/FormTitle.jsx
+++ src/components/FormTitle.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import logo from '../assets/logo.png';
 
-export default function FormTitle({title}) {
+export default function FormTitle({ title }) {
     return (
-        <h1 className="mb-10">{title}</h1>
+        <div>
+            <img src={logo} alt="Logo" className="logo-class" /> {/* Możesz dodać klasę CSS do dostosowania logo */}
+            <h1 className="mb-10">{title}</h1>
+        </div>
     );
 }
