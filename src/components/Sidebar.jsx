810f1c1d89a325cea60cb660315a882ba9d08faa
Nirmean <ana_noona@live.com>
2024-03-05 22:23:53 +0200

    added links to previous pages in side bar and a welcome to your dashboard message with some styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,42 +6,40 @@ import profileImage from "../Assets/jonas-kakaroto.jpg"; // Import the profile p
 const Sidebar = () => {
 
   const user = {
-    username: "John",
+    username: "John Doe",
     profilePicture: profileImage,
   };
 
   return (
-    <div className="sidebar text-white h-full w-80 fixed top-0 left-0 bg-customPurple">
+    <div className="sidebar text-white h-full w-90 fixed top-0 left-0 bg-customPurple flex flex-col items-center justify-center p-4">
       {/* User Profile Section */}
-      <div className="p-4">
-        <img
-          src={user.profilePicture}
-          alt={user.username}
-          className="w-40 h-40 rounded-full mx-auto py-3 px-1"
-        />
-        <p className="mb-8">Welcome back {user.username}!</p>
-        
+      <div  className='bg-slate-100 bg-opacity-50 rounded-xl w-64 text-center outline-6 outline-purple-950 p-8 m-8'>
+          <img
+            src={user.profilePicture}
+            alt={user.username}
+            className="w-40 h-40 rounded-full mx-auto py-4 px-1"
+          />
+          <p className="mb-8">{user.username}</p>
+          <h2>Welcome to your dashboard!</h2>
+          <p className="mb-8 p-4">Gain comprehensive insights into your finances, review your latest transactions, and track your progress toward achieving your savings goals. </p>
       </div>
+      
       {/* Navigation Links */}
       <ul>
+        <h3>Need to make changes?</h3>
         <li>
-          <Link to="/overview" className="block py-2 px-4 hover:bg-black hover:bg-opacity-50 hover:rounded-md">
-          <i className="fas fa-home mr-2"></i>Dashboard
+          <Link to="/income" className="block py-2 px-4 hover:bg-black hover:bg-opacity-50 hover:rounded-md">
+          <i className="fas fa-home mr-2"></i>Your Income
           </Link>
         </li>
         <li>
-          <Link to="/reports" className="block py-2 px-4 hover:bg-black hover:bg-opacity-50 hover:rounded-md">
-            Analytics
+          <Link to="/budget" className="block py-2 px-4 hover:bg-black hover:bg-opacity-50 hover:rounded-md">
+            Your Budget
           </Link>
         </li>
         <li>
-          <Link to="/transactions" className="block py-2 px-4 hover:bg-black hover:bg-opacity-50 hover:rounded-md">
-            Transactions
-          </Link>
-        </li>
-        <li>
-          <Link to="/goals" className="block py-2 px-4 hover:bg-black hover:bg-opacity-50 hover:rounded-md">
-            Account
+          <Link to="/savingsgoals" className="block py-2 px-4 hover:bg-black hover:bg-opacity-50 hover:rounded-md">
+            Savings Goals
           </Link>
         </li>
       </ul>
