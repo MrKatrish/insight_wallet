2b32dbc7c671eb3b91bb4e6a0ac1456de358b6eb
Nirmean <ana_noona@live.com>
2024-03-05 22:34:24 +0200

    commented out mid-width from index.css for better page alignment

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -14,36 +14,39 @@ const Dashboard = () => {
         {/* Sidebar */}
         <Sidebar />
 
-        {/* Main Content */}
-        <div className="flex-1 p-10 grid grid-cols-2 grid-rows-3 gap-4">
-          {/* Tile 1 - Bar Chart */}
-          <div className="bg-white rounded-xl shadow-md p-4 col-span-1">
-            <BarChart />
-          </div>
-          
-          {/* Tile 2 - Currency Converter */}
-          <div className="bg-white rounded-xl shadow-md p-4 col-span-1">
-            <CurrencyConverter />
-          </div>
+        {/* Main Content Wrapper */}
+        <div className="flex-1 p-10 pl-40">
+          {/* Main Content */}
+          <div className="bg-white bg-opacity-50 rounded-xl p-10 grid grid-cols-2 grid-rows-3 gap-4">
+            {/* Tile 1 - Bar Chart */}
+            <div className="bg-white rounded-xl shadow-md p-4 col-span-1">
+              <BarChart />
+            </div>
+            
+            {/* Tile 2 - Currency Converter */}
+            <div className="bg-white rounded-xl shadow-md p-4 col-span-1">
+              <CurrencyConverter />
+            </div>
 
-          {/* Tile 3 - Add Budget (Placeholder) */}
-          <div className="bg-white rounded-xl shadow-md p-4 col-span-1">
-            {/* here add AddBudget component*/}
-          </div>
+            {/* Tile 3 - Add Budget (Placeholder) */}
+            <div className="bg-white rounded-xl shadow-md p-4 col-span-1">
+              {/* here add AddBudget component*/}
+            </div>
 
-          {/* Tile 4 - Pie Chart */}
-          <div className="bg-white rounded-xl shadow-md p-4 col-span-1">
-            <Chart />
-          </div>
+            {/* Tile 4 - Pie Chart */}
+            <div className="bg-white rounded-xl shadow-md p-4 col-span-1">
+              <Chart />
+            </div>
 
-          {/* Tile 5 - Last Transactions */}
-          <div className="bg-white rounded-xl shadow-md p-4 col-span-1">
-            <LastTransactions />
-          </div>
+            {/* Tile 5 - Last Transactions */}
+            <div className="bg-white rounded-xl shadow-md p-4 col-span-1">
+              <LastTransactions />
+            </div>
 
-          {/* Tile 6 - Savings Goals (Placeholder) */}
-          <div className="bg-white rounded-xl shadow-md p-4 col-span-1">
-            {/* here add SavingsGoals componendt*/}
+            {/* Tile 6 - Savings Goals (Placeholder) */}
+            <div className="bg-white rounded-xl shadow-md p-4 col-span-1">
+              {/* here add SavingsGoals component*/}
+            </div>
           </div>
         </div>
       </div>
