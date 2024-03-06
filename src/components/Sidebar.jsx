0cd3a39b02bf4f71184a07a72f7ad4498bc5cbbe
Your Name <you@example.com>
2024-03-06 20:47:36 +0000

    sidebarlink

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import UserInput from '../components/UserInput';
 import FormTitle from '../components/FormTitle';
 import FormButton from '../components/FormButton';
@@ -8,6 +8,8 @@ import MyCalendar from './ExternalCalendar';
 import { TbMoneybag } from "react-icons/tb";
 import { GiReceiveMoney } from "react-icons/gi";
 import { TbZoomMoney } from "react-icons/tb";
+
+
 
 function Income() {
   const navigate = useNavigate();
@@ -97,31 +99,32 @@ function Income() {
       <ul className="col-span-1 text-center mt-2 mb-22 grid grid-rows-4">
         <h3 className="mb-2 font-semibold col-span-1">Need to make changes?</h3>
         <li className="mb-1">
-          <Link to="/income" className="block hover:bg-black hover:bg-opacity-50 hover:rounded-md col-span-1"><GiReceiveMoney />Add new income
+          <Link to="/Income" className="block hover:bg-black hover:bg-opacity-50 hover:rounded-md col-span-1"><GiReceiveMoney />Add new income
           </Link>
         </li>
         <li className="mb-1">
-          <Link to="/budget" className="block hover:bg-black hover:bg-opacity-50 hover:rounded-md col-span-1">
+          <Link to="/Budget" className="block hover:bg-black hover:bg-opacity-50 hover:rounded-md col-span-1">
           <TbZoomMoney className="ml-"/> Amend your budget
           </Link>
         </li>
         <li className="mb-1">
-          <Link to="/savingsgoals" className="block hover:bg-black hover:bg-opacity-50 hover:rounded-md col-span-1">
+          <Link to="/SavingsGoals" className="block hover:bg-black hover:bg-opacity-50 hover:rounded-md col-span-1">
           <TbMoneybag /> Set new savings goals
           </Link>
         </li>
       </ul>
       <UserBadge username={user.username} profilePicture={user.profilePicture} />
-    </div>
-  );
-};
 
+      
       <div className='flex flex-col sm:flex-row p-2 justify-center items-center'>
         <label className="text-lg font-medium leading-10 px-4 sm:px-6 py-2 sm:my-2 sm:mx-2 border-0 ring-1 ring-inset ring-customPurple w-full sm:w-48 bg-white rounded-3xl">Total</label>
         <label className="text-lg font-medium leading-10 py-2 border-0 ring-1 ring-inset ring-customPurple w-full sm:w-64 bg-white rounded-3xl mt-2 sm:mt-0">£ {totalIncome.toFixed(2)}</label>
       </div>
-    </>
+    </div>
+
+
+
   );
-}
+};
 
 export default Income;
