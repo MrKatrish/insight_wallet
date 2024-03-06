e4a0316ef27e34652b9a41a4cfa6a8115e1481f0
Nirmean <ana_noona@live.com>
2024-03-06 22:20:14 +0200

    added width and height styling to barchart so it fills full space of div. works great in full screen but not small screen

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -88,7 +88,7 @@ export default function BarChart() {
   }, []);
 
   return (
-    <div>
+    <div style={{ width: '100%', height: '100%' }}>
     <Bar
       options={options}
       data={chartData}
